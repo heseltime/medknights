3cd5aa24b4a52bb2976ef29f68aac693ffa89b5a
marie_antoinette <jack.heseltine@gmail.com>
2020-10-25 17:04:39 +0100

    trailing slash?

--- medknights/settings.py
+++ medknights/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['medknights.herokuapp.com/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['medknights.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
