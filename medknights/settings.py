70654dde499e212c68d28ea4af281b1e31857299
marie_antoinette <jack.heseltine@gmail.com>
2020-10-25 17:50:11 +0100

    https://blog.usejournal.com/deploying-django-to-heroku-connecting-heroku-postgres-fcc960d290d1 db config

--- medknights/settings.py
+++ medknights/settings.py
@@ -9,13 +9,23 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
-import os
+import dj_database_url
+import dotenv
+
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# .env local call, if exists
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -77,13 +87,11 @@ WSGI_APPLICATION = 'medknights.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
+# set the 'default' key using the dj_database_url module
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -143,3 +151,12 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+# Activate Django-Heroku
+
+django_heroku.settings(locals())
+
+# forget about SSL at the last second
+# https://blog.usejournal.com/deploying-django-to-heroku-connecting-heroku-postgres-fcc960d290d1
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
