93aeead6922ba6ac1cb243b02df8b555a9ef0d83
marie_antoinette <jack.heseltine@gmail.com>
2020-10-25 22:15:03 +0100

    proto

--- medknights/urls.py
+++ medknights/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='storefront.html')),
 ]
